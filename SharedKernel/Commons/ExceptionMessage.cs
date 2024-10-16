16a523a2ff4c5855543657dc87b95e3a0821c802
S.M.Owais <smowais.neduet@gmial.com>
2024-10-16 20:53:14 +0200

    minor changes

--- SharedKernel/Commons/ExceptionMessage.cs
+++ SharedKernel/Commons/ExceptionMessage.cs
@@ -4,6 +4,6 @@ public class ExceptionMessage
 {
     public const string RECORD_DOESNOT_EXIST = "The specified record does not exist.";
     public const string RECORD_ALREADY_EXISTS = "The record already exists.";
-    public const string CANNOT_DELETE = "Cannot delete the record because record are associated with this it";
+    public const string CANNOT_DELETE = "Cannot delete the record because record are associated with it.";
     public const string PAGINATION_ERROR = "PageSize and PageNo must be greater than 0.";
 }
